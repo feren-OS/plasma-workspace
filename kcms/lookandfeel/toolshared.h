a1190fc3cc603b06da8ec07c72d1f9a27bb8dd12
Dominic Hayes <ferenosdev@outlook.com>
2022-04-22 22:16:48 +0100

    Import MR https://invent.kde.org/plasma/plasma-workspace/-/merge_requests/1657

---
+++ kcms/lookandfeel/toolshared.h
@@ -0,0 +1,35 @@
+/*
+    SPDX-FileCopyrightText: 2014 Marco Martin <mart@kde.org>
+    SPDX-FileCopyrightText: 2014 Vishesh Handa <me@vhanda.in>
+    SPDX-FileCopyrightText: 2019 Cyril Rossi <cyril.rossi@enioka.com>
+    SPDX-FileCopyrightText: 2021 Benjamin Port <benjamin.port@enioka.com>
+    SPDX-FileCopyrightText: 2022 Dominic Hayes <ferenosdev@outlook.com>
+
+    SPDX-License-Identifier: LGPL-2.0-only
+*/
+
+#ifndef TOOLSHARED_H
+#define TOOLSHARED_H
+
+#include <KConfig>
+#include <KConfigGroup>
+#include <KPackage/Package>
+#include <KService>
+#include <QDir>
+#include <QObject>
+#include "lookandfeelmanager.h"
+
+class LookAndFeelData;
+class LookAndFeelSettings;
+class LookAndFeelManager;
+
+class ToolShared : public QObject
+{
+    Q_OBJECT
+public:
+    int listPackages();
+    int applyPackage(const QString &theme, const LookAndFeelManager::AppearanceToApply &appearanceItems, const LookAndFeelManager::LayoutToApply &layoutItems);
+};
+
+
+#endif // TOOLSHARED_H
